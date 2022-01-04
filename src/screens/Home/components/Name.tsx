de52aedc0a64e58075882ce70880fb2ecbcb9f5b
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-01-04 20:20:07 +0200

    feat: typewriter name effect

--- src/screens/Home/components/Name.tsx
+++ src/screens/Home/components/Name.tsx
@@ -1,19 +1,33 @@
 import { useEffect } from "react";
 import { FC, useState } from "react";
 
+const NAME = "Hugo Hutri";
+
+/**
+ * Get a random delay for the typewriter effect (100ms - 200ms)
+ * @returns Time in milliseconds
+ */
+const getRandomDelay = () => 100 + Math.random() * 100;
+
+const waitRandomDelay = async () => {
+  return new Promise((resolve) => setTimeout(resolve, getRandomDelay()));
+};
+
+/**
+ * My name with typewriter effect
+ */
 const Name: FC<{}> = () => {
   const [cursorPosition, setCursorPosition] = useState(0);
 
+  // Move the cursor
   useEffect(() => {
-    setTimeout(
-      () => setCursorPosition(cursorPosition + 1),
-      100 + Math.random() * 100
-    );
+    if (cursorPosition > NAME.length) return;
+    waitRandomDelay().then(() => setCursorPosition(cursorPosition + 1));
   }, [cursorPosition]);
 
-  return (
-    <div className="name">{"Hugo Hutri".substring(0, cursorPosition)}</div>
-  );
+  const partOfName = NAME.substring(0, cursorPosition);
+
+  return <div className="name">{partOfName}</div>;
 };
 
 export default Name;
