c9f87beb842b1e35a94f330f90782481913e3183
hugohutri <55588133+hugohutri@users.noreply.github.com>
2022-01-06 12:33:26 +0200

    fix: remove centering

--- src/screens/Home/components/ButtonRow.tsx
+++ src/screens/Home/components/ButtonRow.tsx
@@ -1,21 +1,15 @@
 import { FC } from "react";
-import styled from "styled-components";
 import Button from "../../../components/Button";
-
-const StyledButtonRow = styled.div`
-  display: flex;
-  justify-content: center;
-`;
 
 const ButtonRow: FC<{}> = () => {
   return (
-    <StyledButtonRow className="row">
+    <div className="row">
       <Button href="https://github.com/hugohutri">Projektit</Button>
       <Button href="https://github.com/hugohutri/hugohutri/blob/main/README.md">
         Taidot
       </Button>
       <Button disabled>Ota yhteyttä</Button>
-    </StyledButtonRow>
+    </div>
   );
 };
 
